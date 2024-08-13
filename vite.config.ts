df0f300889b7a19886a9022782fd49b36d2d9baf
Juanfran <juanfran.ag@gmail.com>
2024-08-13 08:41:19 +0200

    feat: live reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
+import livePreview from "vite-live-preview";
 
 export default defineConfig({
+  plugins: [
+    livePreview({
+      reload: true,
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
